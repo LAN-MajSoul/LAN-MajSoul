02ca2b9fc44bc5c2e348a35f8f8cf5ae503b0965
shiyihang2007 <467557146@qq.com>
2023-10-18 13:13:34 +0800

    将模板函数的实现放入头文件中

--- include/logger.hpp
+++ include/logger.hpp
@@ -1,6 +1,7 @@
 #ifndef _LOGGER_HPP_
 #define _LOGGER_HPP_
 
+#include <iostream>
 #include <string>
 
 #define contextInfo                                                           \
@@ -8,16 +9,52 @@
 	 std::to_string(__LINE__) + "\e[0m-(\e[1m" + (__FUNCTION__) + "\e[0m)-")
 
 class Logger {
-	template <typename First> void output(First) const;
-	template <typename First, typename Rest> void output(First, Rest...) const;
+	template <typename First> void output(First &&first) const;
+	template <typename First, typename... Rest>
+	void output(First &&first, Rest &&...rest) const;
 
   public:
-	template <typename Tp> void trace(Tp...) const;
-	template <typename Tp> void debug(Tp...) const;
-	template <typename Tp> void info(Tp...) const;
-	template <typename Tp> void warn(Tp...) const;
-	template <typename Tp> void error(Tp...) const;
+	template <typename... Tp> void trace(Tp &&...args) const;
+	template <typename... Tp> void debug(Tp &&...args) const;
+	template <typename... Tp> void info(Tp &&...args) const;
+	template <typename... Tp> void warn(Tp &&...args) const;
+	template <typename... Tp> void error(Tp &&...args) const;
 };
+
+template <typename First> void Logger::output(First &&first) const {
+	std::cerr << first;
+}
+template <typename First, typename... Rest>
+void Logger::output(First &&first, Rest &&...rest) const {
+	output(first), output(rest...);
+}
+
+template <typename... Tp> void Logger::trace(Tp &&...args) const {
+	this->output("[\e[36mtrace\e[0m]"), this->output(": ");
+	this->output("\e[1m"), this->output(std::forward<Tp>(args)...),
+		this->output("\e[0m\n");
+}
+template <typename... Tp> void Logger::debug(Tp &&...args) const {
+	this->output("[\e[35mdebug\e[0m]"), this->output(": ");
+	this->output("\e[1m"), this->output(std::forward<Tp>(args)...),
+		this->output("\e[0m\n");
+}
+template <typename... Tp> void Logger::info(Tp &&...args) const {
+	this->output("[\e[34minfo\e[0m]"), this->output(": ");
+	this->output("\e[1m"), this->output(std::forward<Tp>(args)...),
+		this->output("\e[0m\n");
+}
+template <typename... Tp> void Logger::warn(Tp &&...args) const {
+	this->output("[\e[33mwarn\e[0m]"), this->output(": ");
+	this->output("\e[1m"), this->output(std::forward<Tp>(args)...),
+		this->output("\e[0m\n");
+}
+template <typename... Tp> void Logger::error(Tp &&...args) const {
+	this->output("[\e[31merror\e[0m]"), this->output(": ");
+	this->output("\e[1m"), this->output(std::forward<Tp>(args)...),
+		this->output("\e[0m\n");
+}
+
 extern Logger logger;
 
 #endif
--- source/logger.cpp
+++ source/logger.cpp
@@ -1,33 +1,3 @@
 #include "logger.hpp"
-#include <iostream>
-
-template <typename First> void Logger::output(First first) const {
-	std::cerr << first;
-}
-template <typename First, typename Rest>
-void Logger::output(First first, Rest rest...) const {
-	output(first), output(rest);
-}
-
-template <typename Tp> void Logger::trace(Tp args...) const {
-	this->output("[\e[36mtrace\e[0m]"), this->output(": ");
-	this->output("\e[1m"), this->output(args), this->output("\e[0m\n");
-}
-template <typename Tp> void Logger::debug(Tp args...) const {
-	this->output("[\e[35mdebug\e[0m]"), this->output(": ");
-	this->output("\e[1m"), this->output(args), this->output("\e[0m\n");
-}
-template <typename Tp> void Logger::info(Tp args...) const {
-	this->output("[\e[34minfo\e[0m]"), this->output(": ");
-	this->output("\e[1m"), this->output(args), this->output("\e[0m\n");
-}
-template <typename Tp> void Logger::warn(Tp args...) const {
-	this->output("[\e[33mwarn\e[0m]"), this->output(": ");
-	this->output("\e[1m"), this->output(args), this->output("\e[0m\n");
-}
-template <typename Tp> void Logger::error(Tp args...) const {
-	this->output("[\e[31merror\e[0m]"), this->output(": ");
-	this->output("\e[1m"), this->output(args), this->output("\e[0m\n");
-}
 
 Logger logger;
